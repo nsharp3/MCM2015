19ce50432b0bc84e26da11369f70f7201ccbb64b
Nick <nsharp3@vt.edu>
2015-02-07 02:45:07 -0500

    Began work on main code

---
+++ code/planes.py
@@ -0,0 +1,43 @@
+# MCM 2015 Submission
+#
+# Authors:
+# Nicholas Sharp (nsharp3@vt.edu)
+# Brendan Avent
+# Saurav Sharma
+
+def main():
+
+    ## Problem parameters
+    
+    # The intended flight
+    source.lat = 4.7
+    source.lon = 3.6
+
+    target.lat = 15.6
+    target.lon = 12.3
+    
+    # The problem domain and resolution
+    
+
+    # Do stuff
+    
+
+
+# Calculate the cost of searching an area
+# Returns a lat x lon array with the cost
+# of searching each area
+def calc_costs():
+
+    print("=== Calculating costs")
+
+# Calculate the probability that the plane is in each area
+# Returns a lat x lon array with the likelihood that the plane
+# crashed in that area
+def calc_init_values():
+    
+    print("=== Calculating values")
+
+
+
+if __name__ == "__main__":
+    main()
