67f2f3efe4ac0b4b6f0be60bb129061b0793ea52
Nick <nsharp3@vt.edu>
2015-02-07 23:32:29 -0500

    initial work on guided crashing

--- code/planes.py
+++ code/planes.py
@@ -48,8 +48,8 @@ def main():
     lat_max = 20
     lon_min = 90
     lon_max = 120
-    lat_res = 100
-    lon_res = 100
+    lat_res = 300
+    lon_res = 300
     dim = Dimension_l(lat_min, lat_max, lat_res, lon_min, lon_max, lon_res)
     
     # create Basemap instance.
@@ -84,7 +84,7 @@ def main():
     fig = plt.figure(figsize=(8,8))
     ax = fig.add_axes([0.1,0.1,0.8,0.8])    
    
-    #m.fillcontinents(zorder=1)
+    m.fillcontinents(zorder=1)
     m.drawcoastlines()
     m.drawcountries()
     parallels = np.arange(0.,90,10.)
@@ -193,7 +193,7 @@ def mask_and_normalize_probs(dim_m, probs, m):
 
 def calc_init_values_unguided(dim, source, target):
 
-    print("=== Calculating unguided values")
+    print("=== Calculating unguided probabilities")
 
     p_x = np.linspace(dim.x_min, dim.x_max, dim.x_res)
     p_y = np.linspace(dim.y_min, dim.y_max, dim.y_res)
@@ -224,6 +224,94 @@ def calc_init_values_unguided(dim, source, target):
 
             prob = (dAlongLine / flightLine.length) * \
                 (distrib.cdf(dist + 0.5 * dPerpLine) - distrib.cdf(dist - 0.5 * dPerpLine))
+
+            probs[i,j] = prob
+
+    return probs
+
+
+def calc_init_values_guided(dim, source, target, airports):
+
+    print("=== Calculating guided probabilities")
+
+    nLinePts = 1000 # Constant controlling granularity along line for computation
+
+    p_x = np.linspace(dim.x_min, dim.x_max, dim.x_res)
+    p_y = np.linspace(dim.y_min, dim.y_max, dim.y_res)
+
+    probs = np.zeros((dim.x_res, dim.y_res))
+
+    flightLine = LineString([(source.x, source.y), (target.x, target.y)])
+    
+    # Compute area statistics along each element
+    dX = p_x[1] - p_x[0]
+    dY = p_y[1] - p_y[0]
+
+    # Transform the element areas to be along the line
+    th = np.arctan2(abs(target.y - source.y), abs(source.x - target.x))
+    dAlongLine = np.cos(th)*dX + np.sin(th)*dY
+    dPerpLine = np.sin(th)*dX + np.cos(th)*dY
+
+    distrib = scipy.stats.norm(0, unguided_crash_dev)
+ 
+    lineX = np.linspace(source.x, target.x, nLinePts)
+    lineY = np.linspace(source.y, target.y, nLinePts)
+
+    # Helper function to find the closest airport to a point
+    def closest_airport(pInd):
+
+        minDist = float('inf')
+        minPort = -1
+
+        for ind,apt in enumerate(airports):
+
+            dX = lineX[pInd] - apt.x
+            dY = lineY[pInd] - apt.y
+            dist = sqrt(dX*dX + dY*dY)
+            if(dist < minDist):
+                minDist = dist
+                minPort =    ind
+
+        return minPort
+
+    # Helper function to process a subset of the line with a single
+    # closest airport
+    def process_segment(startInd, endInd, airport):
+
+        print("hi mom")
+        
+
+
+    # Walk along the points of the line, stopping to process each
+    # segment for which a given airport is closest.
+    rangeStartPt = 0
+    currPt = 1
+    prevClosestAirport = closest_airport(0)
+    while True:
+
+        # Process until we see the closest airport change
+        while(closest_airport(currPt) == prevClosestAirport and currPt != (nLinePts-1) ):
+            currPt += 1
+
+        process_segment(rangeStartPt, currPt, prevClosestAirport)
+
+        rangeStartPt = currPt
+        prevClosestAirport = closest_airport(currPt)
+        currPt += 1
+
+
+
+    for iLinePt in range(1,nLinePts):
+
+
+
+    for i in range(dim.x_res):
+        for j in range(dim.y_res):
+            
+            # Coordinates of this point
+            pX = p_x[i]
+            pY = p_x[j]
+
 
             probs[i,j] = prob
 
