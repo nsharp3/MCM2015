657d9828407f8836b2b46899be62f52af3b7d02e
Brendan Avent <bavent@vt.edu>
2015-02-09 20:30:05 -0500

    Cleaned repo structure

--- airport shit/citiesToLatLong.py
+++
@@ -1,9 +0,0 @@
-from geopy.geocoders import Nominatim
-geolocator = Nominatim()
-with open('cities.txt') as f:
-	for line in f:
-		try:
-			location = geolocator.geocode(line)
-			print((location.latitude, location.longitude))
-		except:
-			pass
--- airport shit/parse.py
+++
@@ -1,7 +0,0 @@
-import re
-
-with open('busiestCities.txt') as f:
-	for line in f:
-		l = re.sub(r'\(\w*\)', '', line)
-		print(l[:-1])
-
